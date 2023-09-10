1ac04ac9ef648defb7dbaee09764b594e3a942d6
Amir Panahandeh <amirpanahandeh@yahoo.com>
2023-09-10 23:03:16 +0330

    Update generators to return logits

--- x_transformers/generators.py
+++ x_transformers/generators.py
@@ -6,66 +6,61 @@ from .utils import subsequent_mask, scaled_softmax
 
 def generate(model, x, iterations, temperature=None, top_p=None, top_k=None):
     if temperature is None:
-        generator = Greedy(model)
+        generator = Greedy(model, temperature, top_p, top_k)
     else:
-        generator = Sampler(model)
-    return generator(x, iterations, temperature=temperature, top_p=top_p, top_k=top_k)
+        generator = Sampler(model, temperature, top_p, top_k)
+    return generator(x, iterations)
 
 
 class _Generator(ABC):
-    def __init__(self, model) -> None:
+    def __init__(self, model, temperature, top_p, top_k) -> None:
         super(ABC).__init__()
         self.model = model
+        self.temperature = temperature
+        self.top_p = top_p
+        self.top_k = top_k
+
+    def __call__(self, x, iterations):
+        all_logits = []
+        for _ in range(iterations):
+            logits, probs = self.model(x, subsequent_mask(x.size(1)))
+            logits, probs = logits[0, -1], probs[0, -1]
+            next_word = self.choose_next_word(logits, probs)
+            x = torch.cat([x, torch.zeros(1, 1).to(
+                torch.int64).fill_(next_word)], dim=1)
+            all_logits.append(logits.unsqueeze(0))
+        return torch.cat(all_logits), x
 
     @abstractmethod
-    def __call__(self, x, iterations, temperature=None, top_p=None, top_k=None):
+    def choose_next_word(self, logits, probs):
         pass
-
-    @staticmethod
-    def get_updated_x(x, next_token):
-        return torch.cat([x, torch.zeros(1, 1).to(torch.int64).fill_(next_token)], dim=1)
 
 
 class Greedy(_Generator):
-    def __init__(self, model) -> None:
-        super().__init__(model)
+    def __init__(self, model, temperature, top_p, top_k) -> None:
+        super().__init__(model, temperature, top_p, top_k)
 
-    def generate_probs(self, x):
-        return self.model(x, subsequent_mask(x.size(1)))[1]
-
-    def __call__(self, x, iterations, temperature=None, top_p=None, top_k=None):
-        for _ in range(iterations):
-            probs = self.generate_probs(x)
-            _, next_word = torch.max(probs[0, -1], dim=0)
-            x = _Generator.get_updated_x(x, next_word)
-        return x
+    def choose_next_word(self, _, probs):
+        return torch.max(probs, dim=0)[1]
 
 
 class Sampler(_Generator):
-    def __init__(self, model) -> None:
-        super().__init__(model)
+    fill = -1e9
 
-    def generate_logits(self, x):
-        return self.model(x, subsequent_mask(x.size(1)))[0]
+    def __init__(self, model, temperature, top_p, top_k) -> None:
+        super().__init__(model, temperature, top_p, top_k)
 
-    def __call__(self, x, iterations, temperature=1, top_p=None, top_k=None):
-        fill = -1e9
-        for _ in range(iterations):
-            logits = self.generate_logits(x)
-            logits = logits[0, -1]
-            if top_p is not None:
-                sorted_logits, sorted_indices = torch.sort(
-                    logits, descending=True)
-                cumulative_probs = torch.cumsum(
-                    softmax(sorted_logits, dim=-1), dim=-1)
-                sorted_indices_to_remove = cumulative_probs > top_p
-                indices_to_remove = sorted_indices_to_remove.scatter(
-                    -1, sorted_indices, sorted_indices_to_remove)
-                logits[indices_to_remove] = fill
-            if top_k is not None:
-                logits[logits < torch.sort(
-                    logits, descending=True)[0][top_k].unsqueeze(dim=-1)] = fill
-            probs = scaled_softmax(logits, temperature)
-            next_token = torch.multinomial(probs, num_samples=1)[0]
-            x = _Generator.get_updated_x(x, next_token)
-        return x
+    def choose_next_word(self, logits, _):
+        if self.top_p is not None:
+            sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+            cumulative_probs = torch.cumsum(
+                softmax(sorted_logits, dim=-1), dim=-1)
+            sorted_indices_to_remove = cumulative_probs > self.top_p
+            indices_to_remove = sorted_indices_to_remove.scatter(
+                -1, sorted_indices, sorted_indices_to_remove)
+            logits[indices_to_remove] = self.fill
+        if self.top_k is not None:
+            logits[logits < torch.sort(
+                logits, descending=True)[0][self.top_k].unsqueeze(dim=-1)] = self.fill
+        probs = scaled_softmax(logits, self.temperature)
+        return torch.multinomial(probs, num_samples=1)[0]
